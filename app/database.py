19b79a0211be2aefcb084e38a2c724cd7f271624
Zahid Hernandez <hzahidhg@gmail.com>
2024-10-07 16:19:15 -0600

    Configuraciones de la base de datos y migraciones con alembic

---
+++ app/database.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+load_dotenv()
+
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASS')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+
+database = (f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
+
+engine = create_engine(database)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = Session()
+    try:
+        return db
+    finally:
+        db.close()
