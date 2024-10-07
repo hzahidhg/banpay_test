ef645dad6d879a8816452127aa434c4ba98cb82e
Zahid Hernandez <hzahidhg@gmail.com>
2024-10-07 14:00:08 -0600

    Se agrega requirements del proyecto y configuración base

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
